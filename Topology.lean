4a820dee089fe797458e6620540b842ec3bcc460
LeFerre42 <leferre.craftard@gmail.com>
2024-03-26 11:22:29 +0100

    debut de la completude ( definitions et preuves simples)

--- Topology.lean
+++ Topology.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Topology` library.
 -- Import modules here that should be built as part of the library.
 import «Topology».Basic
+import Topology.Completeness
